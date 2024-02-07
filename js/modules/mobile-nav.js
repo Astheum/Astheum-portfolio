1d58ae8a6288bd484960a092f6a494c49e25cba9
Astheum | Matheus Ferreira <119074740+Astheum@users.noreply.github.com>
2024-02-07 13:48:27 -0300

    Update html e JS module

--- js/modules/mobile-nav.js
+++ js/modules/mobile-nav.js
@@ -1,8 +1,14 @@
 export default function menuToggle() {
     const menu_btn = document.querySelector('.menu');
     const mobile_menu = document.querySelector('.mobile-nav')
+    const contact_btn = document.querySelector('.about-btn')
 
-        menu_btn.addEventListener('click',()=>{
+    menu_btn.addEventListener('click',()=>{
+        menu_btn.classList.toggle('is-active')
+        mobile_menu.classList.toggle('is-active')
+    })
+
+    contact_btn.addEventListener('click',()=>{
         menu_btn.classList.toggle('is-active')
         mobile_menu.classList.toggle('is-active')
     })
